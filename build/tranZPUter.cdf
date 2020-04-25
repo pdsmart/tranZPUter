6c8e6f906b39a0c12c8b8301ed3f82b1bcd8fcfa
Philip Smart <philip.smart@net2net.org>
2020-04-25 23:06:35 +0100

    Migrated initial software away and merged into zOS repository

---
+++ build/tranZPUter.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 17.1.1 Internal Build 593 12/11/2017 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10CL025YU256) Path("/dvlp/Projects/dev/github/tranZPUter/build/") File("tranZPUter.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- software/asm/monitor_SA1510.asm
+++
@@ -1,8 +0,0 @@
-; Configurable parameters.
-COLW:   EQU     40                      ; Width of the display screen (ie. columns).
-ROW:    EQU     25                      ; Number of rows on display screen.
-SCRNSZ: EQU     COLW * ROW              ; Total size, in bytes, of the screen display area.
-SCRLW:  EQU     COLW / 8                ; Number of 8 byte regions in a line for hardware scroll.
-MODE80C:EQU     0
-
-		INCLUDE "sa1510.asm"
